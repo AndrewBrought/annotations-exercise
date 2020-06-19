ee5fabfc2400f0254954e8ac8d63a58570097efe
Andrew Brought <andrewbrought@gmail.com>
2020-06-19 12:04:19 -0500

    walkthrough of exercise

--- src/Author.java
+++ src/Author.java
@@ -3,7 +3,7 @@
  */
 import java.util.ArrayList;
 import java.util.List;
-
+@SuppressWarnings("unchecked")
 public class Author extends Person {
     private List books;
 
@@ -16,22 +16,21 @@ public class Author extends Person {
      * @deprecated Use publishedBooks instead
      */
     @Deprecated
-    @SuppressWarnings({"unchecked"})
+
     public List<String> getBooks() {
         return books;
     }
 
-    @SuppressWarnings("unchecked")
+
     public List<String> publishedBooks() {
         return books;
     }
-    @SuppressWarnings("unchecked")
+
     public void addBook(String book) {
         books.add(book);
     }
 
     @Override
-    @SuppressWarnings("all")
     public String fullName() {
         return String.format("%s, %s", lastName, firstName);
     }
